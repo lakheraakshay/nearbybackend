a4189dca084f2447c92dfbe313aaab950cb2b399
unknown <lakheraakshay@gmail.com>
2022-01-10 00:04:58 +0530

    Near By users

--- Routes/Location_Routes.js
+++ Routes/Location_Routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
 const USER = require("../Schema/AuthSchema");
+const { calc_distance } = require("./Function");
 
 router.post("/set", async (req, res) => {
   try {
@@ -17,5 +18,38 @@ router.post("/set", async (req, res) => {
     console.log(e);
   }
 });
+router.get("/from_distance/:curr_lat/:curr_lon/:range", async (req, res) => {
+  try {
+    const { curr_lat, curr_lon, range } = req.params;
+    const users = await USER.find();
+    const near_by = [];
+    users.map((user) => {
+      var R = 6371; // km
+      var dLat = toRad(user.location.lat - curr_lat);
+      var dLon = toRad(user.location.long - curr_lon);
+      var lat1 = toRad(curr_lat);
+      var lat2 = toRad(user.location.lat);
+
+      var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.sin(dLon / 2) *
+          Math.sin(dLon / 2) *
+          Math.cos(lat1) *
+          Math.cos(lat2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      var d = R * c;
+      console.log(d);
+      if (parseInt(d)+1<=range) {
+        near_by.push(user);
+      }
+    });
+    res.status(200).send({ near_by, users });
+  } catch (e) {
+    console.log(e);
+  }
+});
+function toRad(Value) {
+  return (Value * Math.PI) / 180;
+}
 
 module.exports = router;
