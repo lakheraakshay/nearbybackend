0460e08dd0f435a40565931ae9a711d6420f4ab3
unknown <lakheraakshay@gmail.com>
2022-01-14 13:10:11 +0530

    adding parameter

--- Routes/Auth_Routes.js
+++ Routes/Auth_Routes.js
@@ -95,15 +95,14 @@ router.post("/login_temp", async (req, res) => {
     });
   }
 });
-router.post("/login", async (req, res) => {
+router.post("/login/:email", async (req, res) => {
   try {
-    res
-      .status(200)
-      .send({
-        success: true,
-        msg: "getting data to check",
-        data_received: req.body,
-      });
+    res.status(200).send({
+      success: true,
+      msg: "getting data to check",
+      data_received: req.body,
+      params: req.params,
+    });
   } catch (e) {
     console.log(e);
   }
