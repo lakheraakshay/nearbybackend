d5cc9e079720bee21f0ce014c761dba664444bdf
lakheraakshay <72579940+lakheraakshay@users.noreply.github.com>
2022-02-12 10:30:09 +0530

    ok

--- Routes/Auth_Routes.js
+++ Routes/Auth_Routes.js
@@ -152,7 +152,7 @@ router.patch("/update/:user_id", async (req, res) => {
     console.log(req.params);
     const updated_user = await USER.findByIdAndUpdate(
       req.params.user_id,
-      { userName: "akshaylakhera9dd2" },
+      { ...data },
       { new: true }
     );
     res.status(200).send({
